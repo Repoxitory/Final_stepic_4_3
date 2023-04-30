140ce6aa1f8ba9b0d535a11d687b8838558529d0
Vorozheikin <pavel.vorozheykin@gmail.com>
2023-04-30 20:53:35 +0700

    Add marks "need_review" to tests

--- test_product_page.py
+++ test_product_page.py
@@ -9,6 +9,7 @@ from .pages.product_page import ProductPage
 from .pages.basket_page import BasketPage
 
 
+@pytest.mark.need_review
 @pytest.mark.parametrize('link',
                              ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
                               "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
@@ -91,6 +92,7 @@ def test_guest_should_see_login_link_on_product_page(browser):
     page.should_be_login_link()
 
 
+@pytest.mark.need_review
 def test_guest_can_go_to_login_page_from_product_page(browser):
     link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
     page = ProductPage(browser, link)
@@ -98,6 +100,7 @@ def test_guest_can_go_to_login_page_from_product_page(browser):
     page.go_to_login_page()
 
 
+@pytest.mark.need_review
 def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
     """
         1. Гость открывает страницу товара
@@ -129,6 +132,7 @@ class TestUserAddToBasketFromProductPage:
         page.should_be_authorized_user()
         time.sleep(3)
 
+    @pytest.mark.need_review
     def test_user_can_add_product_to_basket(self, browser):
         """
             0. Регистрация (реализация в фикстуре setup)
