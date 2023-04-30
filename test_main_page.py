784c97451aa49d42098d89a8d4758adf9f139225
Vorozheikin <pavel.vorozheykin@gmail.com>
2023-04-30 18:16:05 +0700

    Create first PageObject pseudo-test

---
+++ test_main_page.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from .pages.main_page import MainPage
+
+
+def test_guest_can_go_to_login_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)
+    page.open()
+    page.go_to_login_page()
