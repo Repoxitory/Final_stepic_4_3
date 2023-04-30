ff54e1b1298b4b7a8a034ad62bd503bf2453f485
Vorozheikin <pavel.vorozheykin@gmail.com>
2023-04-30 20:44:08 +0700

    Add register page locators

--- pages/locators.py
+++ pages/locators.py
@@ -4,6 +4,13 @@
 from selenium.webdriver.common.by import By
 
 
+class BasePageLocators:
+    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+    LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")
+    BASKET_LINK = (By.CSS_SELECTOR, "#default > header > div.page_inner > div > div.basket-mini.pull-right.hidden-xs > span > a")
+    USER_ICON = (By.CSS_SELECTOR, ".icon-user")
+
+
 class MainPageLocators:
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
 
@@ -20,3 +27,15 @@ class ProductPageLocators:
     BASKET_PRICE = (By.XPATH, "//*[@id='default']/header/div[1]/div/div[2]")
     MESSAGES = (By.CSS_SELECTOR, "#messages > div:nth-child(1) > div > strong")
     SUCCESS_MESSAGE = (By.CSS_SELECTOR, ".alert-success")
+
+
+class BasketPageLocators:
+    BASKET_ITEMS = (By.CSS_SELECTOR, "div.basket-items")
+    BASKET_EMPTY_MESSAGE = (By.CSS_SELECTOR, "#content_inner > p")
+
+
+class RegisterPageLocators:
+    EMAIL = (By.CSS_SELECTOR, "#id_registration-email")
+    PASSWORD1 = (By.CSS_SELECTOR, "#id_registration-password1")
+    PASSWORD2 = (By.CSS_SELECTOR, "#id_registration-password2")
+    REGISTER_BUTTON = (By.CSS_SELECTOR, "#register_form > button")
