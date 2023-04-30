86d3d7c4693419761562ec9ef1662571fc6e2c45
Vorozheikin <pavel.vorozheykin@gmail.com>
2023-04-30 19:59:08 +0700

    Add two main tests to their own class with their own mark

--- test_main_page.py
+++ test_main_page.py
@@ -1,23 +1,27 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+import pytest
+
 from .pages.main_page import MainPage
 from .pages.login_page import LoginPage
 from .pages.basket_page import BasketPage
 
 
-def test_guest_can_go_to_login_page_old(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
-    page = MainPage(browser, link)
-    page.open()
-    page.go_to_login_page()
+@pytest.mark.login_guest
+class TestLoginFromMainPage():
 
+    def test_guest_can_go_to_login_page_old(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/"
+        page = MainPage(browser, link)
+        page.open()
+        page.go_to_login_page()
 
-def test_guest_should_see_login_link(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
-    page = MainPage(browser, link)
-    page.open()
-    page.should_be_login_link()
+    def test_guest_should_see_login_link(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/"
+        page = MainPage(browser, link)
+        page.open()
+        page.should_be_login_link()
 
 
 def test_guest_can_go_to_login_page(browser):
