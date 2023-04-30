5269fcee7f9687433425c2093b3ec97b22bc1773
Vorozheikin <pavel.vorozheykin@gmail.com>
2023-04-30 18:29:19 +0700

    Introduce Locators!

--- pages/locators.py
+++ pages/locators.py
@@ -4,26 +4,5 @@
 from selenium.webdriver.common.by import By
 
 
-class BasePageLocators:
+class MainPageLocators():
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
-    LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")
-    BASKET_LINK = (By.CSS_SELECTOR, "#default > header > div.page_inner > div > div.basket-mini.pull-right.hidden-xs > span > a")
-
-
-class BasketPageLocators(BasePageLocators):
-    BASKET_ITEMS = (By.CSS_SELECTOR, "div.basket-items")
-    BASKET_EMPTY_MESSAGE = (By.CSS_SELECTOR, "#content_inner > p")
-
-
-class LoginPageLocators(BasePageLocators):
-    LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
-    REGISTER_FORM = (By.CSS_SELECTOR, "#register_form")
-
-
-class ProductPageLocators(BasePageLocators):
-    BUTTON = (By.CSS_SELECTOR, "button.btn-add-to-basket")
-    PRODUCT_NAME = (By.CSS_SELECTOR, "#content_inner > article > div.row > div.col-sm-6.product_main > h1")
-    PRODUCT_PRICE = (By.CSS_SELECTOR, "#content_inner > article > div.row > div.col-sm-6.product_main > p.price_color")
-    BASKET_PRICE = (By.XPATH, "//*[@id='default']/header/div[1]/div/div[2]")
-    MESSAGES = (By.CSS_SELECTOR, "#messages > div:nth-child(1) > div > strong")
-    SUCCESS_MESSAGE = (By.CSS_SELECTOR, ".alert-success")
